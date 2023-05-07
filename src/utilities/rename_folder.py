0185e84da914845c8a76e333fd283378fd063abb
Beamlnwza <Supawit04591@pcccr.ac.th>
2023-05-07 11:19:44 +0700

    fix files name to make it numeric order

--- src/utilities/rename_folder.py
+++ src/utilities/rename_folder.py
@@ -1,20 +1,24 @@
 import os
 
 
-def rename_folder():
-    print("rename folder in process")
-    path = os.path.join(os.getcwd(), "data", "raw")
-    for i in os.listdir(path):
-        os.rename(os.path.join(path, i), os.path.join(path, i.zfill(2)))
+def rename_02():
+    """
+    try to rename folder with zfill(2)
+    """
 
-
-def rename_files():
-    print("rename files in process")
-    path = os.path.join(os.getcwd(), "data", "raw")
-    for i in os.listdir(path):
-        for j in os.listdir(os.path.join(path, i)):
-            os.rename(os.path.join(path, i, j), os.path.join(path, i, j.zfill(5)))
+    print("Rename Process")
+    raw_path = os.path.abspath(os.getcwd() + "/data/raw")
+    for i in os.listdir(raw_path):
+        folder_path = raw_path + "/" + i
+        files_path = [x for x in os.listdir(folder_path) if x.endswith(".jpg")]
+        for j in files_path:
+            name_no_jpg = j.split(".")[0]
+            name_no_jpg = name_no_jpg.zfill(2)
+            os.rename(
+                folder_path + "/" + j,
+                folder_path + "/" + name_no_jpg + ".jpg"
+            )
 
 
 if __name__ == "__main__":
-    rename_files()
+    rename_02()
